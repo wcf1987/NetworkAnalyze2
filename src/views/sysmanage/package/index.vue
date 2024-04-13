166c409657dd2fed1a32bcbf8045f012efadee80
ice_home <181712814@qq.com>
2024-04-13 23:36:39 +0800

    封装应用头增加瀑布显示功能

--- src/views/sysmanage/package/index.vue
+++ src/views/sysmanage/package/index.vue
@@ -22,8 +22,16 @@
                     </el-icon>
                     批量删除
                 </el-button>
+
+              <el-button size="default" type="warning" class="ml10" @click="onChangeView()">
+                <el-icon>
+                  <ele-Memo/>
+                </el-icon>
+                <div>{{state.viewStr }}</div>
+              </el-button>
             </div>
-            <el-table :data="state.tableData.data" v-loading="state.tableData.loading" style="width: 100%" @selection-change="handleSelectionChange">
+
+            <el-table :data="state.tableData.data" v-loading="state.tableData.loading" style="width: 100%" @selection-change="handleSelectionChange" v-if="state.view=='list'">
                <el-table-column type="selection" width="30"/>
                 <el-table-column prop="ID" label="ID" width="60" v-if="false"/>
                 <el-table-column type="index" label="序号" width="60" :index="calcIndex"/>
@@ -48,7 +56,7 @@
                         </el-button>
                     </template>
                 </el-table-column>
-            </el-table>
+            </el-table >
             <el-pagination
                     @size-change="onHandleSizeChange"
                     @current-change="onHandleCurrentChange"
@@ -60,8 +68,82 @@
                     v-model:page-size="state.tableData.param.pageSize"
                     layout="total, sizes, prev, pager, next, jumper"
                     :total="state.tableData.total"
+                    v-if="state.view=='list'"
             >
             </el-pagination>
+          <div class="waterfall-first" v-if="state.view=='waterfall'">
+            <div class="waterfall-first-item"   v-for="(v, k) in state.tableData.data" :key="k"   v-waves  @click="onOpenEditDetail('edit', v)">
+              <div class="w100 h100 flex">
+                <div class="item-txt">
+                  <div class="item-txt-title">
+                    <div class="item-txt-msg mr10">
+                      <span> {{ v.Name }}</span>
+
+                    </div>
+
+                  </div>
+                  <div class="item-txt-title">
+                    <div style="width: 100% "  >
+
+                      <div class="item-txt-msg mb10 ">
+                        <span class="centered">类型： {{ v.Type }}</span>
+
+                      </div>
+
+
+
+                      <div class="item-txt-msg mb10 ">
+                        <el-tooltip
+                            class="box-item"
+                            effect="light"
+                            content="编辑"
+                            placement="bottom-start"
+                        >
+                          <el-button type="success" size="small" circle
+                                     @click.stop="onOpenEdit('edit', v)">
+                            <el-icon>
+                              <ele-Document/>
+                            </el-icon>
+
+                          </el-button>
+                        </el-tooltip>
+                        <el-tooltip
+                            class="box-item"
+                            effect="light"
+                            content="删除"
+                            placement="bottom-start"
+                        >
+                          <el-button type="danger" size="small" circle
+                                     @click.stop="onRowDel(v)">
+                            <el-icon>
+                              <ele-Delete/>
+                            </el-icon>
+
+                          </el-button>
+                        </el-tooltip>
+                        <el-tooltip
+                            class="box-item"
+                            effect="light"
+                            content="修改"
+                            placement="bottom-start"
+                        >
+                          <el-button type="warning" size="small" circle
+                                     @click.stop="onOpenEditDetail('edit', v)">
+                            <el-icon>
+                              <ele-Edit/>
+                            </el-icon>
+
+                          </el-button>
+                        </el-tooltip>
+
+
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </el-card>
         <UserDialog ref="userDialogRef" @refresh="getTableData()" @editdetail="onOpenEditDetailByID"/>
     </div>
@@ -79,7 +161,10 @@
     // 定义变量内容
     const userDialogRef = ref();
     const state = reactive({
+      view:'list',
+      viewStr:'切换瀑布流显示',
         tableData: {
+
             data: [],
             total: 0,
             loading: false,
@@ -200,6 +285,22 @@
         state.tableData.searchStr=state.tableData.search;
        getTableData();
     };
+    //切换瀑布视图和列表展示
+    const onChangeView = () => {
+      if(state.view=='list'){
+        state.view='waterfall';
+        state.viewStr='切换列表显示';
+        state.tableData.param.pageSize = 100;
+        getTableData();
+      }else{
+        state.view='list';
+
+        state.viewStr='切换瀑布流显示';
+        state.tableData.param.pageSize = 10;
+        getTableData();
+      }
+
+    };
     const onOpenEditDetail = (type: string, row: RowUserType) => {
         onOpenEditDetailByID(row.ID,row.Describes);
 
@@ -275,5 +376,157 @@
                 flex: 1;
             }
         }
+      .waterfall-first {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(188px, 1fr));
+        grid-gap: 0.25em;
+        grid-auto-flow: row dense;
+        grid-auto-rows: 20px;
+        .waterfall-first-item {
+          width: 100%;
+          background: var(--el-color-primary);
+          color: var(--el-color-white);
+          transition: all 0.3s ease;
+          border-radius: 3px;
+          &:nth-of-type(3n + 1) {
+            grid-row: auto / span 5;
+          }
+          &:nth-of-type(3n + 2) {
+            grid-row: auto / span 6;
+          }
+          &:nth-of-type(3n + 3) {
+            grid-row: auto / span 8;
+          }
+          &:hover {
+            box-shadow: 0 2px 12px 0 rgb(0 0 0 / 10%);
+            transition: all 0.3s ease;
+            cursor: pointer;
+          }
+        }
+      }
+      .waterfall-last {
+        display: grid;
+        grid-gap: 0.25em;
+        grid-auto-flow: row dense;
+        grid-auto-rows: minmax(188px, 20vmin);
+        grid-template-columns: 1fr;
+        .waterfall-last-item {
+          height: 100%;
+          background: var(--el-color-primary);
+          color: var(--el-color-white);
+          transition: all 0.3s ease;
+          border-radius: 3px;
+          &:hover {
+            box-shadow: 0 2px 12px 0 rgb(0 0 0 / 10%);
+            transition: all 0.3s ease;
+            cursor: pointer;
+          }
+        }
+      }
+      @media (min-width: 576px) {
+        .waterfall-last {
+          grid-template-columns: repeat(7, 1fr);
+          .waterfall-last-item {
+            &:nth-of-type(9n + 9) {
+              grid-column: auto / span 2;
+            }
+            &:nth-of-type(9n + 8) {
+              grid-column: auto / span 2;
+            }
+            &:nth-of-type(9n + 7) {
+              grid-column: auto / span 3;
+            }
+            &:nth-of-type(9n + 6) {
+              grid-column: auto / span 2;
+            }
+            &:nth-of-type(9n + 5) {
+              grid-column: auto / span 3;
+            }
+            &:nth-of-type(9n + 4) {
+              grid-column: auto / span 2;
+            }
+            &:nth-of-type(9n + 3) {
+              grid-column: auto / span 3;
+            }
+            &:nth-of-type(9n + 2) {
+              grid-column: auto / span 2;
+            }
+            &:nth-of-type(9n + 1) {
+              grid-column: auto / span 2;
+            }
+          }
+        }
+      }
+      @media (min-width: 576px) and (min-width: 1024px) {
+        .waterfall-last {
+          grid-template-columns: repeat(14, 1fr);
+          .waterfall-last-item {
+            &:nth-of-type(15n + 15) {
+              grid-column: auto / span 3;
+            }
+            &:nth-of-type(15n + 14) {
+              grid-column: auto / span 3;
+            }
+            &:nth-of-type(15n + 13) {
+              grid-column: auto / span 2;
+            }
+            &:nth-of-type(15n + 12) {
+              grid-column: auto / span 3;
+            }
+            &:nth-of-type(15n + 11) {
+              grid-column: auto / span 3;
+            }
+            &:nth-of-type(15n + 10) {
+              grid-column: auto / span 2;
+            }
+            &:nth-of-type(15n + 9) {
+              grid-column: auto / span 3;
+            }
+            &:nth-of-type(15n + 8) {
+              grid-column: auto / span 3;
+            }
+            &:nth-of-type(15n + 7) {
+              grid-column: auto / span 3;
+            }
+            &:nth-of-type(15n + 6) {
+              grid-column: auto / span 3;
+            }
+            &:nth-of-type(15n + 5) {
+              grid-column: auto / span 3;
+            }
+            &:nth-of-type(15n + 4) {
+              grid-column: auto / span 3;
+            }
+            &:nth-of-type(15n + 3) {
+              grid-column: auto / span 3;
+            }
+            &:nth-of-type(15n + 2) {
+              grid-column: auto / span 3;
+            }
+            &:nth-of-type(15n + 1) {
+              grid-column: auto / span 2;
+            }
+          }
+        }
+      }
+      .item-txt {
+        flex: 1;
+        padding: 15px;
+        display: flex;
+        flex-direction: column;
+        overflow: hidden;
+
+        .item-txt-title {
+          text-overflow: ellipsis;
+          overflow: hidden;
+          -webkit-line-clamp: 2;
+          -webkit-box-orient: vertical;
+          display: -webkit-box;
+          color: #f3f0de;
+          transition: all 0.3s ease;
+
+
+        }
+      }
     }
 </style>
