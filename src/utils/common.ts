128ba4d8d5d85822544d3000c946a424c73ebca5
icefish <181712814@qq.com>
2024-07-12 18:00:59 +0800

    增加地址管理中ip图示

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,16 +1,16 @@
 // 导航数据
 export const FieldType = [
-    {id: 1, label: 'int8_t', length: 1,value:'int8_t'},
-    {id: 2, label: 'int16_t', length: 2,value:'int16_t'},
-    {id: 3, label: 'int32_t', length: 4,value:'int32_t'},
-    {id: 4, label: 'int64_t', length: 8,value:'int64_t'},
-    {id: 5, label: 'uint8_t', length: 1,value:'uint8_t'},
-    {id: 6, label: 'uint16_t', length: 2,value:'uint16_t'},
-    {id: 7, label: 'uint32_t', length: 4,value:'uint32_t'},
-    {id: 8, label: 'uint64_t', length: 8,value:'uint64_t'},
-    {id: 9, label: 'float', length: 4,value:'float'},
-    {id: 10, label: 'double', length: 8,value:'double'},
-    {id: 11, label: 'bit', length: 1,value:'bit'},
+    {id: 1, label: 'int8_t', length: 1, value: 'int8_t'},
+    {id: 2, label: 'int16_t', length: 2, value: 'int16_t'},
+    {id: 3, label: 'int32_t', length: 4, value: 'int32_t'},
+    {id: 4, label: 'int64_t', length: 8, value: 'int64_t'},
+    {id: 5, label: 'uint8_t', length: 1, value: 'uint8_t'},
+    {id: 6, label: 'uint16_t', length: 2, value: 'uint16_t'},
+    {id: 7, label: 'uint32_t', length: 4, value: 'uint32_t'},
+    {id: 8, label: 'uint64_t', length: 8, value: 'uint64_t'},
+    {id: 9, label: 'float', length: 4, value: 'float'},
+    {id: 10, label: 'double', length: 8, value: 'double'},
+    {id: 11, label: 'bit', length: 1, value: 'bit'},
 
 ];
 export const NetworkProtocol = [
@@ -19,14 +19,41 @@ export const NetworkProtocol = [
 
 
 ];
+export const IPDevicePic = [
+    {id: 1, src: '/src/assets/ipdevices/camera.png', text: '摄像头'},
+    {id: 2, src: '/src/assets/ipdevices/dmjs.png', text: '卫星接收'},
+
+    {id: 3, src: '/src/assets/ipdevices/dp.png', text: '火箭炮'},
+
+    {id: 4, src: '/src/assets/ipdevices/firewall.png', text: '防火墙'},
+
+    {id: 5, src: '/src/assets/ipdevices/fly1.png', text: '战斗机'},
+
+    {id: 6, src: '/src/assets/ipdevices/fly2.png', text: '运输机'},
+
+    {id: 7, src: '/src/assets/ipdevices/fwq.png', text: '服务器'},
+    {id: 8, src: '/src/assets/ipdevices/home.png', text: '家庭接入'},
+
+    {id: 9, src: '/src/assets/ipdevices/network.png', text: '网络中接'},
+
+    {id: 10, src: '/src/assets/ipdevices/pc.png', text: '电脑'},
+
+    {id: 11, src: '/src/assets/ipdevices/qst.png', text: '潜水艇'},
+
+    {id: 12, src: '/src/assets/ipdevices/tank.png', text: '坦克'},
+
+    {id: 13, src: '/src/assets/ipdevices/wrj.png', text: '无人机'},
+    {id: 22, src: '/src/assets/ipdevices/weizhi.png', text: '未知设备'},
+];
+
 export const FunctionType = [
-    {id: '三角函数', label: '三角函数', value: '三角函数',children:[]},
-    {id: '字符串计算', label: '字符串计算', value: '字符串计算',children:[]},
-    {id: '日期计算', label: '日期计算', value: '日期计算',children:[]},
-    {id: '类型转换', label: '类型转换', value: '类型转换',children:[]},
-    {id: '列表函数', label: '列表函数', value: '列表函数',children:[]},
-    {id: '字典函数', label: '字典函数', value: '字典函数',children:[]},
-{id: '自定义函数', label: '自定义函数', value: '自定义函数',children:[]},
+    {id: '三角函数', label: '三角函数', value: '三角函数', children: []},
+    {id: '字符串计算', label: '字符串计算', value: '字符串计算', children: []},
+    {id: '日期计算', label: '日期计算', value: '日期计算', children: []},
+    {id: '类型转换', label: '类型转换', value: '类型转换', children: []},
+    {id: '列表函数', label: '列表函数', value: '列表函数', children: []},
+    {id: '字典函数', label: '字典函数', value: '字典函数', children: []},
+    {id: '自定义函数', label: '自定义函数', value: '自定义函数', children: []},
 
 
 ];
@@ -38,8 +65,8 @@ export const PackageHeader = [
 export const MessHeader = [
     {id: 1, label: '固定长度', value: '固定长度'},
     {id: 2, label: 'S', value: 'S'},
-     {id: 3, label: 'K', value: 'K'},
-     {id: 4, label: 'Z', value: 'Z'},
+    {id: 3, label: 'K', value: 'K'},
+    {id: 4, label: 'Z', value: 'Z'},
     {id: 5, label: '应用层协议头', value: '应用层协议头'},
 
 ];
@@ -59,13 +86,11 @@ export const DataType = [
     {id: 2, label: '47数据', value: '47数据'},
 
 
-
 ];
 export const ActionType = [
     {id: 0, label: '发送数据', value: '发送数据'},
     {id: 1, label: '延缓执行', value: '延缓执行'},
     {id: 2, label: '其他1', value: '其他1'},
-
 
 
 ];
@@ -74,13 +99,12 @@ export const FilterType = [
     {id: 1, label: '丢弃', value: '丢弃'},
 
 
-
 ];
 export const MessPackProperty = [
     {id: 0, label: '无', value: '无'},
     {id: 1, label: '网管', value: '网管'},
     {id: 2, label: '端机', value: '端机'},
-{id: 3, label: '47001', value: '47001'},
+    {id: 3, label: '47001', value: '47001'},
     {id: 4, label: '运管', value: '运管'},
 
 ];
@@ -97,8 +121,8 @@ export const VarType = [
 export const SpecailNodeType = [
     {id: 0, label: '定时器节点', value: '定时器节点'},
     {id: 1, label: '数据统计节点', value: '数据统计节点'},
- {id: 2, label: '计算节点', value: '计算节点'},
-     {id: 3, label: '延时器节点', value: '延时器节点'},
+    {id: 2, label: '计算节点', value: '计算节点'},
+    {id: 3, label: '延时器节点', value: '延时器节点'},
     {id: 4, label: '消息队列节点', value: '消息队列节点'},
-       {id: 4, label: '过滤器节点', value: '过滤器节点'},
+    {id: 4, label: '过滤器节点', value: '过滤器节点'},
 ];
--- src/views/sysmanage/address/networkinterface/dialog.vue
+++ src/views/sysmanage/address/networkinterface/dialog.vue
@@ -1,222 +1,242 @@
 <template>
-    <div class="system-user-dialog-container">
-        <el-dialog :title="state.dialog.title" v-model="state.dialog.isShowDialog" width="769px" :draggable=true>
-            <el-form ref="userDialogFormRef" :model="state.ruleForm" :rules="state.baseRules" size="default"
-                     label-width="90px">
-                <el-row :gutter="35">
-                    <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="mb20">
-                        <el-form-item label="名称" prop="Name">
-                            <el-input v-model="state.ruleForm.Name" placeholder="请输入地址名称" clearable></el-input>
-                        </el-form-item>
-                    </el-col>
+  <div class="system-user-dialog-container">
+    <el-dialog :title="state.dialog.title" v-model="state.dialog.isShowDialog" width="769px" :draggable=true>
+      <el-form ref="userDialogFormRef" :model="state.ruleForm" :rules="state.baseRules" size="default"
+               label-width="90px">
+        <el-row :gutter="35">
+          <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="mb20">
+            <el-form-item label="名称" prop="Name">
+              <el-input v-model="state.ruleForm.Name" placeholder="请输入地址名称" clearable></el-input>
+            </el-form-item>
+          </el-col>
 
-                    <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="mb20">
-                        <el-form-item label="类型" prop="Type">
-                            <el-input v-model="state.ruleForm.Type" placeholder="网口" clearable
-                                      :readonly=true></el-input>
-                        </el-form-item>
-                    </el-col>
+          <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="mb20">
+            <el-form-item label="类型" prop="Type">
+              <el-input v-model="state.ruleForm.Type" placeholder="网口" clearable
+                        :readonly=true></el-input>
+            </el-form-item>
+          </el-col>
 
-                    <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="mb20">
-                        <el-form-item label="IP" prop="IP">
-                            <el-input v-model="state.ruleForm.IP" placeholder="ip地址" clearable></el-input>
-                        </el-form-item>
-                    </el-col>
+          <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="mb20">
+            <el-form-item label="IP" prop="IP">
+              <el-input v-model="state.ruleForm.IP" placeholder="ip地址" clearable></el-input>
+            </el-form-item>
+          </el-col>
 
-                    <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="mb20">
-                        <el-form-item label="端口" prop="Port">
-                            <el-input v-model="state.ruleForm.Port" placeholder="" clearable></el-input>
-                        </el-form-item>
-                    </el-col>
+          <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="mb20">
+            <el-form-item label="端口" prop="Port">
+              <el-input v-model="state.ruleForm.Port" placeholder="" clearable></el-input>
+            </el-form-item>
+          </el-col>
 
 
-                    <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="mb20">
-                        <el-form-item label="协议" prop="Protocol">
-                            <el-select v-model="state.ruleForm.Protocol" placeholder="请选择" clearable class="w100">
-                                <el-option
-                                        v-for="item in NetworkProtocolOptions"
-                                        :key="item.id"
-                                        :label="item.label"
-                                        :value="item.value"
-                                />
-                            </el-select>
-                        </el-form-item>
-                    </el-col>
+          <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="mb20">
+            <el-form-item label="协议" prop="Protocol">
+              <el-select v-model="state.ruleForm.Protocol" placeholder="请选择" clearable class="w100">
+                <el-option
+                    v-for="item in NetworkProtocolOptions"
+                    :key="item.id"
+                    :label="item.label"
+                    :value="item.value"
+                />
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="12" :md="12" :lg="12" :xl="12" class="mb20">
+            <el-form-item label="图示" prop="Pic">
+              <el-select v-model="state.ruleForm.Pic" placeholder="请选择" clearable class="w100" @change="changeSelection">
+                <el-option
+                    v-for="item in IPDevicePicOptions"
+                    :key="item.id"
 
-                    <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24" class="mb20">
-                        <el-form-item label="用户描述" prop="Describes">
-                            <el-input v-model="state.ruleForm.Describes" type="textarea" placeholder="请输入用户描述"
-                                      maxlength="150"></el-input>
-                        </el-form-item>
-                    </el-col>
-                </el-row>
-            </el-form>
-            <template #footer>
+                    :value="item.text"
+                >
+                  <img :src="item.src" style="width: 16px;height: 16px;vertical-align: text-bottom;">
+
+                </el-option>
+              </el-select>
+            </el-form-item>
+          </el-col>
+          <el-col :xs="24" :sm="24" :md="24" :lg="24" :xl="24" class="mb20">
+            <el-form-item label="用户描述" prop="Describes">
+              <el-input v-model="state.ruleForm.Describes" type="textarea" placeholder="请输入用户描述"
+                        maxlength="150"></el-input>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form>
+      <template #footer>
 				<span class="dialog-footer">
 					<el-button @click="onCancel" size="default">取 消</el-button>
 					<el-button type="primary" @click="onSubmit" size="default">{{ state.dialog.submitTxt }}</el-button>
 				</span>
-            </template>
-        </el-dialog>
-    </div>
+      </template>
+    </el-dialog>
+  </div>
 </template>
 
 <script setup lang="ts" name="systemUserDialog">
-    import {nextTick, reactive, ref} from 'vue';
-    import {NetworkProtocol} from '/@/utils/common';
-    import {addressApi} from '/@/api/sysmanage/address';
-    import {ElMessage} from "element-plus";
-    import {useUserInfo} from "/@/stores/userInfo";
+import {nextTick, reactive, ref} from 'vue';
+import {NetworkProtocol, IPDevicePic} from '/@/utils/common';
+import {addressApi} from '/@/api/sysmanage/address';
+import {ElMessage} from "element-plus";
+import {useUserInfo} from "/@/stores/userInfo";
 
-    const NetworkProtocolOptions = ref(NetworkProtocol);
-    // 定义子组件向父组件传值/事件
-    const emit = defineEmits(['refresh']);
+const NetworkProtocolOptions = ref(NetworkProtocol);
+const IPDevicePicOptions = ref(IPDevicePic)
+// 定义子组件向父组件传值/事件
+const emit = defineEmits(['refresh']);
 
-    // 定义变量内容
-    const userDialogFormRef = ref();
-    const rules = reactive({
+// 定义变量内容
+const userDialogFormRef = ref();
+const rules = reactive({
 // 普通的校验规则
-        name: [
-            {required: true, message: '名称不能为空'},
-            {min: 1, max: 10, message: '名称长度为1 - 10位'},
-        ],
+  name: [
+    {required: true, message: '名称不能为空'},
+    {min: 1, max: 10, message: '名称长度为1 - 10位'},
+  ],
+});
+import {telValidator, checkInterNum, checkIdNum, checkPort, checkIP} from '/@/utils/rules'
+
+const state = reactive({
+  ruleForm: {
+    Name: '', // 账户名称
+    Type: '', // 用户昵称
+    IP: '',
+    Port: '',
+    Protocol: '',
+    describe: '', // 用户描述
+  },
+  baseRules: {
+    Name: [{required: true, message: '请输入名称', trigger: 'blur'}],
+    Protocol: [{required: true, message: '请选择协议', trigger: 'change'}],
+    IP: [{required: true, message: '请输入IP格式', trigger: 'change', validator: checkIP}],
+    //Port: [{ message: '端口为数字1-65535', trigger: 'change',validator:checkPort }]
+  },
+
+  deptData: [] as DeptTreeType[], // 部门数据
+  dialog: {
+    isShowDialog: false,
+    type: '',
+    title: '',
+    submitTxt: '',
+  },
+});
+const changeSelection=(val)=> {
+
+
+
+
+}
+// 打开弹窗
+const openDialog = (type: string, row: RowUserType) => {
+  state.dialog.isShowDialog = true;
+  state.dialog.type = type;
+  if (type === 'edit') {
+
+    state.dialog.title = '修改';
+    state.dialog.submitTxt = '修 改';
+    nextTick(() => {
+      Object.assign(state.ruleForm, row);
+      //state.ruleForm = row;
     });
-    import {telValidator, checkInterNum, checkIdNum, checkPort, checkIP} from '/@/utils/rules'
-    const state = reactive({
-        ruleForm: {
-            Name: '', // 账户名称
-            Type: '', // 用户昵称
-            IP: '',
-            Port: '',
-            Protocol: '',
-            describe: '', // 用户描述
-        },
-        baseRules: {
-            Name: [{required: true, message: '请输入名称', trigger: 'blur'}],
-            Protocol: [{required: true, message: '请选择协议', trigger: 'change'}],
-            IP: [{required: true, message: '请输入IP格式', trigger: 'change',validator:checkIP }],
-            //Port: [{ message: '端口为数字1-65535', trigger: 'change',validator:checkPort }]
-        },
-
-        deptData: [] as DeptTreeType[], // 部门数据
-        dialog: {
-            isShowDialog: false,
-            type: '',
-            title: '',
-            submitTxt: '',
-        },
+  } else {
+    state.dialog.title = '新增';
+    state.dialog.submitTxt = '新 增';
+    nextTick(() => {
+      userDialogFormRef.value.resetFields();
+      //state.ruleForm = row;
     });
-
-    // 打开弹窗
-    const openDialog = (type: string, row: RowUserType) => {
-        state.dialog.isShowDialog = true;
-        state.dialog.type = type;
-        if (type === 'edit') {
-
-            state.dialog.title = '修改';
-            state.dialog.submitTxt = '修 改';
-            nextTick(() => {
-                Object.assign(state.ruleForm, row);
-                //state.ruleForm = row;
-            });
-        } else {
-            state.dialog.title = '新增';
-            state.dialog.submitTxt = '新 增';
-            nextTick(() => {
-                userDialogFormRef.value.resetFields();
-                //state.ruleForm = row;
-            });
-            state.ruleForm.Type = "网口";
-            // 清空表单，此项需加表单验证才能使用
+    state.ruleForm.Type = "网口";
+    // 清空表单，此项需加表单验证才能使用
 
 
+  }
 
-        }
+  getMenuData();
+};
+// 关闭弹窗
+const closeDialog = () => {
+  state.dialog.isShowDialog = false;
 
-        getMenuData();
-    };
-    // 关闭弹窗
-    const closeDialog = () => {
-        state.dialog.isShowDialog = false;
+};
+// 取消
+const onCancel = () => {
+  closeDialog();
+};
+// 提交
+const onSubmit = () => {
+  // console.log('123123');
+  userDialogFormRef.value.validate((valid) => {
+    // console.log('123123');
+    // 不通过校验
+    if (!valid) {
 
-    };
-    // 取消
-    const onCancel = () => {
-        closeDialog();
-    };
-    // 提交
-    const onSubmit = () => {
-       // console.log('123123');
-        userDialogFormRef.value.validate((valid) => {
-           // console.log('123123');
-            // 不通过校验
-            if (!valid) {
+      return ElMessage.error('请确保数据格式填写正确！');
+    } else {
+      console.log('验证通过');
+      if (state.dialog.type == 'edit') {
+        addressApi().updateNetworkInter(
+            state.ruleForm
+        )
+            .then(res => {
+              //console.log(res);
+              if (res.code == '200') {
 
-                return ElMessage.error('请确保数据格式填写正确！');
-            } else {
-                console.log('验证通过');
-                if (state.dialog.type == 'edit') {
-                    addressApi().updateNetworkInter(
-                        state.ruleForm
-                    )
-                        .then(res => {
-                            //console.log(res);
-                            if (res.code == '200') {
+                ElMessage.success("修改成功");
+                closeDialog();
+                emit('refresh');
 
-                                ElMessage.success("修改成功");
-                                closeDialog();
-                                emit('refresh');
+              } else {
+                ElMessage.error(res.message);
+              }
 
-                            } else {
-                                ElMessage.error(res.message);
-                            }
+            }).catch(err => {
 
-                        }).catch(err => {
+        }).finally(() => {
 
-                    }).finally(() => {
-
-                    });
-                }
-                if (state.dialog.type == 'add') {
-                    const stores = useUserInfo();
-                    state.ruleForm['AuthorID'] = stores.userInfos.id
-                    addressApi().addNetworkInter(
-                        state.ruleForm
-                    )
-                        .then(res => {
-                            //console.log(res);
-                            if (res.code == '200') {
-
-                                ElMessage.success("添加成功");
-                                closeDialog();
-                                emit('refresh');
-
-                            } else {
-                                ElMessage.error(res.message);
-                            }
-
-                        }).catch(err => {
-
-                    }).finally(() => {
-
-                    });
-                }
-
-            }
-            // 通过校验
-            // 登录逻辑
         });
+      }
+      if (state.dialog.type == 'add') {
+        const stores = useUserInfo();
+        state.ruleForm['AuthorID'] = stores.userInfos.id
+        addressApi().addNetworkInter(
+            state.ruleForm
+        )
+            .then(res => {
+              //console.log(res);
+              if (res.code == '200') {
+
+                ElMessage.success("添加成功");
+                closeDialog();
+                emit('refresh');
+
+              } else {
+                ElMessage.error(res.message);
+              }
+
+            }).catch(err => {
+
+        }).finally(() => {
+
+        });
+      }
+
+    }
+    // 通过校验
+    // 登录逻辑
+  });
 };
 
-        // if (state.dialog.type === 'add') { }
+// if (state.dialog.type === 'add') { }
 
-    // 初始化列表数据
-    const getMenuData = () => {
+// 初始化列表数据
+const getMenuData = () => {
 
-    };
+};
 
-    // 暴露变量
-    defineExpose({
-        openDialog,
-    });
+// 暴露变量
+defineExpose({
+  openDialog,
+});
 </script>
